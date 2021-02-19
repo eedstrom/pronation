9db39f4bd93677e01aaa472e1eadcf19d295faad
Eric Edstrom <ericedstrom123@gmail.com>
2021-02-19 14:57:09 -0600

    Added copyright info

--- Python Code/accel_data_parsing.py
+++ Python Code/accel_data_parsing.py
@@ -1,3 +1,20 @@
+# Pronation Program
+# Copyright (C) 2021 Dominic Culotta, Eric Edstrom, Jae Young Lee, Teagan Mathur, Brian Petro, Wilma Rishko, Ruizhi Wang
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import numpy as np
 
 accel_raw = open( 'RAWACCEL.CSV', 'r' ) #read the raw data
@@ -16,6 +33,3 @@ for line in accel_raw.readlines():
     zaccel.append(values[2])
 
 print(zaccel)
-
-
-
