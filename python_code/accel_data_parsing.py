f2c3ec17aaee90abcac7cef6305de44acc68cfe4
Eric Edstrom <ericedstrom123@gmail.com>
2021-02-26 14:44:58 -0600

    Removed capitals

---
+++ python_code/accel_data_parsing.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+# Pronation Program
+# Copyright (C) 2021 Dominic Culotta, Eric Edstrom, Jae Young Lee, Teagan Mathur, Brian Petro, Wilma Rishko, Ruizhi Wang
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import scipy.optimize as sy
+from pathlib import Path
+import sys
+import os
+
+# Load in the path to the data
+p = Path(os.getcwd()) / sys.argv[1]
+
+# Load in the dat
+df = pd.read_csv(p, header=None)
+
+# Give it a header
+df.columns = ["ax", "ay", "az", "gx", "gy", "gz"]
+time = np.linspace(0, len(df['ay']), len(df['ay']))
+
+sns.lineplot(x=time, y=df['az'], linewidth=1, color='r')
+sns.lineplot(x=time, y=df['ax'], linewidth=1)
+sns.lineplot(x=time, y=df['ay'], linewidth=1, color='g')
+
+# making a best fit line to az data
+
+
+def model(x, m, b):
+    return m*x+b
+
+
+intit_guess = [1, 1]
+fit = sy.curve_fit(model, time, df['az'], p0=intit_guess, absolute_sigma=True)
+
+# extracking out m and b
+ans = fit[0]
+slope_fit, y_fit = ans[0], ans[1]
+
+plt.plot(time, model(time, slope_fit, y_fit))
+
+
+plt.show()
---
+++ python_code/video_processing/opencv_timing.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# Pronation Program
+# Copyright (C) 2021 Dominic Culotta, Eric Edstrom, Jae Young Lee, Teagan Mathur, Brian Petro, Wilma Rishko, Ruizhi Wang
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import sys
+import cv2
+import numpy as np
+#print(sys.argv[1])
+
+def GetFrames(sys.argv[1]):
+    vidObj = cv2.VideoCapture(sys.argv[1])
+    
+    count = 0
+    
+    success = 1
+    while success:
+
+        success, image = viObj.read()
+
+        cv2.imwrite("frame%d.jpg" % count, image)
+
+        count += 1
+
+if __name__ == '__main__':
+    
+    GetFrame(sys.argv[1])
