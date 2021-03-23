178a72eb9e429770ea5fa89ba2a62e63d28cfebb
Teagan Mathur <tmathur2@illinois.edu>
2021-03-23 18:12:57 -0500

    Plots quiet stance, walking, and running csv files in one

--- python_code/fsr_data_plotting.py
+++ python_code/fsr_data_plotting.py
@@ -3,48 +3,62 @@
 import csv
 import matplotlib.pyplot as plt
 
-path = '/Volumes/NO NAME/FSR.CSV'
+path = '/Users/teaganmathur/Documents/PHYS398DLP/pronation/python_code/data/'
+filenames = ['QuietStanceFSR.CSV', 'WalkingFSR.CSV', 'RunningFSR.CSV']
+quiet_filename = 'QuietStanceFSR.CSV'
+walk_filename = 'WalkingFSR.CSV'
+run_filename = 'RunningFSR.CSV'
 
-# Read in fsr data
-with open(path, newline='') as csvfile:
-    reader = csv.reader(csvfile)
-    fsr_data = list(reader)
-    fsr_data = fsr_data[1:] # Remove the first row
-    fsr_data = [[float(val) for val in row] for row in fsr_data]
+for filename in filenames:
 
-# Column information for fsr_data
-# fsr (0-3), time_taken, uncertainty_in_time, conductance in micro-mhos
+    # Read in fsr data
+    with open(path + filename, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        fsr_data = list(reader)
+        fsr_data = fsr_data[1:] # Remove the first row
+        fsr_data = [[float(val) for val in row] for row in fsr_data]
 
-fsr0, fsr1, fsr2, fsr3 = [], [], [], []
-t0, t1, t2, t3 = [], [], [], []
+    # Column information for fsr_data
+    # fsr (0-3), time_taken, uncertainty_in_time, conductance in micro-mhos
 
-# Separate fsr data
-for row in fsr_data:
-    fsr, t, dt, cond = row
+    fsr0, fsr1, fsr2, fsr3 = [], [], [], []
+    t0, t1, t2, t3 = [], [], [], []
 
-    if fsr == 0:
-        fsr0.append(cond)
-        t0.append(t)
-    elif fsr == 1:
-        fsr1.append(cond)
-        t1.append(t)
-    elif fsr == 2:
-        fsr2.append(cond)
-        t2.append(t)
-    else:
-        fsr3.append(cond)
-        t3.append(t)
+    # Formula for converting conductance (mhos) to force (lbs)
+    # conductance = 6*10^(-7)*force - 1*10^(-7)
+    # force = (1/6)*10^7*conductance + (1/6)
 
-# Plot the fsr data
-plt.plot(t0, fsr0, label="FSR0")
-plt.plot(t1, fsr1, label="FSR1")
-plt.plot(t2, fsr2, label="FSR2")
-plt.plot(t3, fsr3, label="FSR3")
-plt.xlabel("Time")
-plt.ylabel("Conductance (micro-mhos)")
-plt.grid()
-plt.legend()
-plt.show()
+    # Separate fsr data and convert
+    for row in fsr_data:
+        fsr, t, dt, cond = row
+
+        cond = cond * 10**(-6)   # convert micro mhos to mhos
+        force = (1/6) * 10**7 * cond + (1/6) # convert conductance to force
+
+        if fsr == 0:
+            fsr0.append(force)
+            t0.append(t)
+        elif fsr == 1:
+            fsr1.append(force)
+            t1.append(t)
+        elif fsr == 2:
+            fsr2.append(force)
+            t2.append(t)
+        else:
+            fsr3.append(force)
+            t3.append(t)
+
+    # Plot the fsr data
+    plt.plot(t0, fsr0, label="FSR0")
+    plt.plot(t1, fsr1, label="FSR1")
+    plt.plot(t2, fsr2, label="FSR2")
+    plt.plot(t3, fsr3, label="FSR3")
+    plt.xlabel("Time")
+    plt.ylabel("Force (lbs)")
+    plt.title(filename.split('.')[0])
+    plt.grid()
+    plt.legend()
+    plt.show()
 
 
 
