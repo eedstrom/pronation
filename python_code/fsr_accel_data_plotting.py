86daf4d1b61e283d2c1282bbc26c683c154e03bf
Teagan Mathur <tmathur2@illinois.edu>
2021-04-02 16:07:00 -0500

    Time converted to s and altered for 3.31_Loomis_[1st].csv files

--- python_code/fsr_accel_data_plotting.py
+++ python_code/fsr_accel_data_plotting.py
@@ -8,74 +8,66 @@ from pathlib import Path
 path = Path(os.getcwd()) / "python_code/data/"
 
 # filenames = ['QuietStance.CSV', 'Walking.CSV', 'Running.CSV']
-filenames = ['3.31_Loomis_Data.CSV']
+filenames = ['3.31_Loomis_1st.csv', '3.31_Loomis_2nd.csv', '3.31_Loomis_3rd.csv', '3.31_Loomis_4th.csv', '3.31_Loomis_5th.csv']
 
 for filename in filenames:
     # Read in data
     with open(path / filename) as csvfile:
         reader = csv.reader(csvfile)
-        full_data = list(reader)
-    
-    # Build the data sets into a dictionary
-    # e.g. {1:[[0,0,0],[0,0]], 2:[[0,0,0],[0,0]], ......}
-    datasets = {}
-    setnum = 0
-    for row in full_data:
-        if row[0] == '-1':          # Header row
-            setnum = setnum + 1
-            datasets[setnum] = []   # Initialize new dataset in the dictionary
+        data = list(reader)
+        data = data[1:] # Remove the first row
+        data = [[float(val) for val in row] for row in data]
+
+    # Column information for fsr_data
+    # fsr (0-3), time_taken, uncertainty_in_time, conductance in micro-mhos
+
+    fsr0, fsr1, fsr2, fsr3 = [], [], [], []
+    t0, t1, t2, t3 = [], [], [], []
+    start = 0
+
+    # Formula for converting conductance (mhos) to force (lbs)
+    # conductance = 6*10^(-7)*force - 1*10^(-7)
+    # force = (1/6)*10^7*conductance + (1/6)
+
+    # Separate fsr data from accel data and convert
+    for i in range(len(data)):
+        row = data[i]
+
+        if i == 0:  # find the starting time
+            start = row[1] / 1000.0
+
+        if row[0] > 2:
+            bus, t, dt, cond = row
+            fsr = bus - 3           # bus line 3 = fsr 0
+            t = t / 1000.0 - start  # convert ms to s and set start to 0
+        # accel data rows (bus lines 0-2)
         else:
-            datasets[setnum].append([float(val) for val in row])
+            continue
+        
+        cond = cond * 10**(-6)   # convert micro mhos to mhos
+        force = 10**7 * cond * (1.0/6.0) + (1.0/6.0) # convert conductance to force
 
-    # Loop through each data set in the dictionary
-    for setnum, data in datasets.items():
-        # Column information for fsr_data
-        # fsr (0-3), time_taken, uncertainty_in_time, conductance in micro-mhos
+        if fsr == 0:
+            fsr0.append(force)
+            t0.append(t)
+        elif fsr == 1:
+            fsr1.append(force)
+            t1.append(t)
+        elif fsr == 2:
+            fsr2.append(force)
+            t2.append(t)
+        else:
+            fsr3.append(force)
+            t3.append(t)
 
-        fsr0, fsr1, fsr2, fsr3 = [], [], [], []
-        t0, t1, t2, t3 = [], [], [], []
-
-        test = []
-
-        # Formula for converting conductance (mhos) to force (lbs)
-        # conductance = 6*10^(-7)*force - 1*10^(-7)
-        # force = (1/6)*10^7*conductance + (1/6)
-
-        # Separate fsr data from accel data and convert
-        for row in data:
-            # fsr data rows (bus lines 3-6)
-            if row[0] > 2:
-                bus, t, dt, cond = row
-                fsr = bus - 3   # bus line 3 = fsr 0
-            # accel data rows (bus lines 0-2)
-            else:
-                continue
-            
-            cond = cond * 10**(-6)   # convert micro mhos to mhos
-            force = 10**7 * cond * (1.0/6.0) + (1.0/6.0) # convert conductance to force
-
-            if fsr == 0:
-                fsr0.append(force)
-                t0.append(t)
-            elif fsr == 1:
-                fsr1.append(force)
-                t1.append(t)
-            elif fsr == 2:
-                test.append(cond)
-                fsr2.append(force)
-                t2.append(t)
-            else:
-                fsr3.append(force)
-                t3.append(t)
-
-        # Plot the fsr data
-        plt.plot(t0, fsr0, label="FSR0")
-        plt.plot(t1, fsr1, label="FSR1")
-        plt.plot(t2, fsr2, label="FSR2")
-        plt.plot(t3, fsr3, label="FSR3")
-        plt.xlabel("Time")
-        plt.ylabel("Force (lbs)")
-        plt.title("Data Set {}".format(setnum))
-        plt.grid()
-        plt.legend()
-        plt.show()
+    # Plot the fsr data
+    plt.plot(t0, fsr0, label="FSR0")
+    plt.plot(t1, fsr1, label="FSR1")
+    plt.plot(t2, fsr2, label="FSR2")
+    plt.plot(t3, fsr3, label="FSR3")
+    plt.xlabel("Time (seconds)")
+    plt.ylabel("Force (lbs)")
+    plt.title(filename)
+    plt.grid()
+    plt.legend()
+    plt.show()
